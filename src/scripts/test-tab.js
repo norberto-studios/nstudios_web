b81f8a7445c8fa2e3485fecc65ccddb2407b9aed
Norberto Fernandez <norbertostudios@gmail.com>
2025-08-15 10:52:22 -0400

    added tabs js

--- src/scripts/test-tab.js
+++ src/scripts/test-tab.js
@@ -9,10 +9,27 @@ console.log("tabs: ",tabs);
 console.log("panels: ",panels);
 
 tabs.forEach(t => t.addEventListener('click', () => {
+    const key = t.dataset.key;
+
     tabs.forEach(x => x.setAttribute('aria-selected','false'));
     t.setAttribute('aria-selected','true');
+    // tabs.forEach(x => {
+    //     x.setAttribute('aria-selected', 'false');
+    //     x.classList.remove('bg-[#1a1b21]', 'text-white', 'shadow-[inset_0_0_0_1px_#2b2d35]');
+    //     x.classList.add('text-[#cfd1d6]');
+    // });
+    //
+    // t.setAttribute('aria-selected', 'true');
+    // t.classList.add('bg-[#1a1b21]', 'text-white', 'shadow-[inset_0_0_0_1px_#2b2d35]');
+    // t.classList.remove('text-[#cfd1d6]');
 
-    const id = t.dataset.key
-    Object.values(panels).forEach(p => p.hidden = true);
-    panels[id].hidden = false;
+    // tabs.forEach(x => x.setAttribute('aria-selected','false'));
+    // t.setAttribute('aria-selected','true');
+
+    console.log("dataset: ",t)
+
+
+    Object.entries(panels).forEach(([k, panel]) => {
+        panel.classList.toggle('hidden', k !== key);
+    });
 }));
