886345fd364111fabfa601c27a1dab4401d3211a
Norberto Fernandez <norbertostudios@gmail.com>
2025-08-16 01:49:04 -0400

    testing nginx 07

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,8 +8,7 @@ import svelte from '@astrojs/svelte';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://norberto-studios.github.io',
-  base: '/',
-  // base: '/nstudios_web/',
+  base: '/nstudios_web/',
 
   vite: {
     plugins: [tailwindcss()]
