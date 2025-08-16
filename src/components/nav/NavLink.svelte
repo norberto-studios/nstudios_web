737648ab10420135e1018fed3cd5c91406af4610
Norberto Fernandez <norbertostudios@gmail.com>
2025-08-16 00:20:10 -0400

    limited pages for live deployment

---
+++ src/components/nav/NavLink.svelte
@@ -0,0 +1,11 @@
+<script>
+    let { href, children } = $props();
+</script>
+
+<a class="hover:text-white
+        my-1
+        px-2 rounded"
+    href={href}
+>
+    {@render children?.()}
+</a>
--- src/components/nav/menu.svelte
+++ src/components/nav/menu.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+    import NavLink from "./NavLink.svelte";
+
     let menuOpen = false;
 
     function closeMenu() {
@@ -8,7 +10,8 @@
 
 <div class="flex items-center">
     <!-- Hamburger Icon -->
-    <button class="md:hidden cursor-pointer text-white focus:outline-none" on:click={() => (menuOpen = !menuOpen)}>
+    <button aria-label="hamburger-icon-btn"
+            class="md:hidden cursor-pointer text-white focus:outline-none" onclick={() => (menuOpen = !menuOpen)}>
         <svg class="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <path d="M4 6h16M4 12h16M4 18h16" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
         </svg>
@@ -21,18 +24,22 @@
 			? 'flex flex-col bg-black absolute top-16 left-0 w-full p-6 z-50'
 			: 'hidden'
 	} md:flex gap-6 text-gray-400 items-center`}>
-        <a class="hover:text-red-300" href="/pricing">Pricing</a>
-        <a class="hover:text-white" href="/services">Services</a>
-        <a class="hover:text-white" href="/apps">Apps</a>
-        <a class="hover:text-white" href="/contact">Contact</a>
-        <div class="flex md:flex-row flex-col gap-6 md:pl-6">
-            <a class="hover:text-white" href="/cart">Cart</a>
-            <a class="hover:text-white" href="/login">Login</a>
+        <NavLink href="/services">Services</NavLink>
+        <NavLink href="/pricing">Pricing</NavLink>
+        <NavLink href="/apps">Apps</NavLink>
+        <NavLink href="/contact">Contact</NavLink>
+        <div class="flex md:flex-row flex-col md:pl-6">
+            <NavLink href="/cart">Cart</NavLink>
+            <NavLink href="/login">Login</NavLink>
         </div>
     </nav>
 
     <!-- Overlay -->
     {#if menuOpen}
-        <div class="fixed inset-0  z-40" on:click={closeMenu}></div>
+        <div role="button"
+             tabindex="-1"
+                onkeydown={(e) => e.key === 'Escape' && closeMenu()}
+                class="fixed inset-0  z-40" onclick={closeMenu}>
+        </div>
     {/if}
 </div>
