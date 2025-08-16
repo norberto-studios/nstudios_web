50a34c7118894b4298644d15df2be4e64f9da0fd
Norberto Fernandez <norbertostudios@gmail.com>
2025-08-16 00:41:02 -0400

    changing the base

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,10 @@ import svelte from '@astrojs/svelte';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://norberto-studios.github.io',
-  base: 'nstudios_web',
+  // base: 'nstudios_web',
+  build: {
+    assets: "assets",
+  },
 
   vite: {
     plugins: [tailwindcss()]
